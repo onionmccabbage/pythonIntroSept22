d7202618681536ecc280aff529b4d63b75316aa6
onionmccabbage <onionmccabbage@live.com>
2022-09-20 13:19:15 +0100

    Add files via upload

--- using_requests.py
+++ using_requests.py
@@ -8,8 +8,10 @@ import requests
 # https://jsonplaceholder.typicode.com/users/
 
 def getData():
+    # ask which user to get
+    whichID = input('Which user? ')
     # here is an end-point Application Programming Interface (API)
-    url = 'https://jsonplaceholder.typicode.com/users/'
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(whichID)
     # we can make get, post, put, update and delete calls using the requests library
     results = requests.get(url) # this contains the JSON data
     # convert the results into a dictionary
@@ -20,4 +22,10 @@ def getData():
 if __name__ == '__main__':
     users = getData() # grab all the data from the end-point
     # print some parts of the data using print formatting
-    print(users[0]['address']['geo'])
+    # python will inject the value into the curly brackets
+    print('The user geo is {}'.format( users['address']['geo']) )
+    # find some useful data members from the returned structure
+    username = users['name']
+    website  = users['website']
+    compname = users['company']['name']
+    print('Username: {} Website: {} Company Name: {}'.format( username, website, compname ))
