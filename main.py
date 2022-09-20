44ce280f964a92e09891030fdb7cb12282c2d618
onionmccabbage <onionmccabbage@live.com>
2022-09-20 10:37:00 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # here we ask the user for input then return the integer part of that input
 from using_more_fn import makeInt
 
-
 user_input = input('please type something: ')
 
 # use our imported function to make an integer
